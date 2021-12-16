6bf453aa3d2a668c45247220a36c06e928746ddb
2552nik <nick.muravscky2013@yandex.ru>
2021-12-16 21:25:00 +0300

    add & searsh

---
+++ project/newapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import NewsList, NewsDetailView, NewsAddView, NewsEditView, NewsDeleteView, NewsSearch
+
+
+urlpatterns = [
+    path('', NewsList.as_view()),
+    path('<int:pk>/', NewsDetailView.as_view(), name='news_detail'),  # Ссылка на детали новости
+    path('add/', NewsAddView.as_view(), name='news_add'),  # Ссылка на создание новости
+    path('edit/<int:pk>', NewsEditView.as_view(), name='news_edit'),
+    path('delete/<int:pk>', NewsDeleteView.as_view(), name='news_delete'),
+    path('search/', NewsSearch.as_view(), name='news_searsh'),
+]
